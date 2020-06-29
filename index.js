dae5dbe70161e0f74a46f14cc07637799bc1b813
Emil Bay <github@tixz.dk>
2020-06-30 01:37:16 +0200

    Fix assertions

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const binding = require('node-gyp-build')(__dirname)
 module.exports = class Rabin {
   constructor (min = 512 * 1024, max = 8 * 1024 * 1024, bits = 20) {
     assert(min >>> 0 === min, 'min must be uint 32')
-    assert(max >>> 0 === max, 'min must be uint 32')
+    assert(min > 0, 'min must be greater than 0')
+    assert(max >>> 0 === max, 'max must be uint 32')
+    assert(max > 0, 'max must be greater than 0')
     assert(Number.isInteger(bits), 'bits must be integer')
     assert(bits > 0 && bits <= 53, 'bits must be in (0, 53]')
 
